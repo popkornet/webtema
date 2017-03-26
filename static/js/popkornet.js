d694371f2c95d0acd9aafc129a0458ff89ae0d25
Stian Lode <stian.lode@gmail.com>
2017-03-26 12:56:41 +0200

    Sets up a shop page.

--- static/js/popkornet.js
+++ static/js/popkornet.js
@@ -28,7 +28,7 @@ $(function() {
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-    target: '.navbar-fixed-top'
+    target: '.navbar-shrinkable'
 })
 
 // Closes the Responsive Menu on Menu Item Click
@@ -42,4 +42,21 @@ $(document).ready(function() {
         document.getElementById('focusme').focus();
     });
 
-
+simpleCart({
+  checkout: {
+    type: "Custom"
+  },
+  cartColumns: [
+      { attr: "name" , label: "Tittel" } ,
+      { attr: "price" , label: "Pris", view: 'currency' } ,
+      { view: "decrement" , label: false , text: "<span class='fa fa-fw fa-minus'></span>" } ,
+      { attr: "quantity" , label: "Antall" } ,
+      { view: "increment" , label: false , text: "<span class='fa fa-fw fa-plus'></span>" } ,
+      { attr: "total" , label: "Subtotal", view: 'currency' } ,
+      { view: "remove" , text: "<span class='fa fa-fw fa-close text-danger'></span>" , label: false }
+  ],
+  currency: "NOK",
+  cartStyle: "table",
+  cartClass: "table table-responsive table-condensed table-inverse",
+  shippingQuantityRate: 40,
+});
